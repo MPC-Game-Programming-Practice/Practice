ca16fcefafba1f3008c917260a10679ebe9501d3
hushojin <52961461+hushojin@users.noreply.github.com>
2021-08-29 06:21:41 +0900

    Update Ans2.cpp

--- Day3-2/Ans2.cpp
+++ Day3-2/Ans2.cpp
@@ -1,1 +1,22 @@
+# include <Siv3D.hpp>
 
+void Main()
+{
+	Array<Circle> circles;
+	circles << Circle(Scene::Center(), 50);
+	while (System::Update())
+	{
+		for (auto circle : circles) {
+			if (circle.leftClicked()) {
+				circles.remove(circle);
+				circles << Circle(circle.center.x + circle.r, circle.y, circle.r);
+				circles << Circle(circle.center.x - circle.r, circle.y, circle.r);
+				break;
+			}
+		}
+		for (auto it = circles.end();it!=circles.begin();) {
+			it--;
+			it->draw(Palette::Blue).drawFrame();
+		}
+	}
+}
