49a62b8b40d4c83557321f934f7b4636fbf8d3cc
SeiyaT <65097781+SeiyaTk@users.noreply.github.com>
2021-08-30 08:13:04 +0900

    Update ans2.cpp

--- Day4-1/ans2.cpp
+++ Day4-1/ans2.cpp
@@ -1,13 +1,21 @@
-
 # include <Siv3D.hpp>
 
 void Main()
 {
-  //自分の画像のパスを入力する
-	const Texture MPC(U"C:/Users/SeiyaT/source/repos/OpenSiv3D(0.4.3)1/MPC.png", TextureDesc::Mipped);
-	while (System::Update()) {
-		const double scale = 0.02 + Periodic::Triangle0_1(10s) * 0.4;
-		MPC.scaled(scale).drawAt(200, 300);
-		MPC.scaled(scale).rotated(Scene::Time() * 90_deg).drawAt(400, 300);
-	}
+    Scene::SetBackground(ColorF(0.8, 0.9, 1.0));
+
+    const Texture sample(U"C:/Users/SeiyaT/source/repos/OpenSiv3D(0.4.3)/OpenSiv3D(0.4.3)/Image/Sample.png",  TextureDesc::Mipped);
+    //Pathは各自で適するものを用いる。
+
+    while (System::Update())
+    {
+        // 2 倍に拡大して描画
+        sample.resized(160).drawAt(200, 300);
+
+        // 1 倍で描画
+        sample.resized(80).drawAt(400, 300);
+
+        // 半分のサイズに縮小したもの中心を軸に回転させて描画
+        sample.resized(40).rotated(Scene::Time()*90_deg).drawAt(600, 300);
+    }
 }
